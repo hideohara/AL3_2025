8f128ad493e6183b854e9cfe8c41c27abe9d5cdb
ohara <ohara@g.neec.ac.jp>
2025-04-11 19:11:28 +0900

    01_03.エンジンの使い方

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,9 +2,28 @@
 
 using namespace KamataEngine;
 
+// デストラクタ
+GameScene::~GameScene()
+{
+	delete model_;
+}
+
 // 初期化
 void GameScene::Initialize()
 {
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// カメラの初期化
+	camera_.Initialize();
+
+
+
 }
 
 // 更新
@@ -15,4 +34,21 @@ void GameScene::Update()
 // 描画
 void GameScene::Draw()
 {
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// 3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	//ここに3Dモデルインスタンスの描画処理を記述する
+	// 
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	// 3Dモデル描画後処理
+	Model::PostDraw();
+
+
+
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -7,6 +7,9 @@ class GameScene
 {
 
 public:
+    // デストラクタ
+    ~GameScene();
+
     // 初期化
     void Initialize();
 
@@ -15,6 +18,23 @@ public:
 
     // 描画
     void Draw();
+
+private:
+    // テクスチャハンドル
+    uint32_t textureHandle_ = 0;
+
+    // 3Dモデル
+    KamataEngine::Model* model_ = nullptr;
+
+    // ワールドトランスフォーム
+    KamataEngine::WorldTransform worldTransform_;
+    // カメラ
+    KamataEngine::Camera camera_;
+
+
+
+
+
 
 };
 
