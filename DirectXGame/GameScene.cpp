2b13ef8dbf4ca505915f0ca2bca6958c3b3f7009
ohara <ohara@g.neec.ac.jp>
2025-04-11 19:47:51 +0900

    修正２

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,9 +2,21 @@
 
 using namespace KamataEngine;
 
+// デストラクタ
+GameScene::~GameScene()
+{
+	delete model_;
+}
+
 // 初期化
 void GameScene::Initialize()
 {
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	// 3Dモデルの生成
+	model_ = Model::Create();
+	// カメラの初期化
+	camera_.Initialize();
 }
 
 // 更新
