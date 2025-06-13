1af4fad15c259a9f72de6f50355dd9fcbdbb0922
ohara <ohara@g.neec.ac.jp>
2025-06-13 13:58:20 +0900

    02_07.マップチップとの当たり判定（天井）_修正版

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -220,15 +220,14 @@ void Player::CheckMapCollisionUp(CollisionMapInfo& info)
 	// ブロックにヒット？
 	if (hit) {
 		// めり込みを排除する方向に移動量を設定する
-		indexSet = mapChipField_->GetMapChipIndexSetByPosition(worldTransform_.translation_ + Vector3(0, +kHeight / 2.0f, 0));
+		// indexSet = mapChipField_->GetMapChipIndexSetByPosition(worldTransform_.translation_ + Vector3(0, +kHeight / 2.0f, 0));
+		indexSet = mapChipField_->GetMapChipIndexSetByPosition(worldTransform_.translation_ + info.move + Vector3(0, +kHeight / 2.0f, 0));
 		// めり込み先ブロックの範囲矩形
 		MapChipField::Rect rect = mapChipField_->GetRectByIndex(indexSet.xIndex, indexSet.yIndex);
 		info.move.y = std::max(0.0f, rect.bottom - worldTransform_.translation_.y - (kHeight / 2.0f + kBlank));
 		// 天井に当たったことを記録する
 		info.ceiling = true;
 	}
-
-
 }
 
 // ③判定結果を反映して移動させる
